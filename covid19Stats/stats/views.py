7c0255bd3cddae9c9dc852303d61f281629bbf0b
Lemaire Lucas <lucas_lemaire@ens.univ-artois.fr>
2020-04-14 21:12:39 +0200

    [ADD] navbar + stats

--- covid19Stats/stats/views.py
+++ covid19Stats/stats/views.py
@@ -1,3 +1,22 @@
+from django.views.generic import DetailView, ListView
 from django.shortcuts import render
+from django.db.models import Sum
+from .models import Stats
 
-# Create your views here.
+
+class DashboardView(ListView):
+    model = Stats
+    template_name = 'dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        all_stats_object = Stats.objects.get(country='All')
+        context['total_cases'] = all_stats_object.total_cases
+        context['total_deaths'] = all_stats_object.total_deaths
+        context['recovered_cases'] = all_stats_object.recovered_cases
+
+        return context
+    
+class MapView(ListView):
+    model = Stats
+    template_name = 'map.html'
