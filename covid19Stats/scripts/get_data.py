00e3f4b43013e59f95a6c664f9a113f9a1bf8189
Lemaire Lucas <lucas_lemaire@ens.univ-artois.fr>
2020-04-14 16:35:23 +0200

    base project

---
+++ covid19Stats/scripts/get_data.py
@@ -0,0 +1,39 @@
+import http.client
+import json
+import sqlite3
+import os
+from stats.models import Stats
+
+def run():
+
+    conn = http.client.HTTPSConnection("covid-193.p.rapidapi.com")
+
+    headers = {
+    'x-rapidapi-host': "covid-193.p.rapidapi.com",
+    'x-rapidapi-key': "5bffb4691cmsh45cb38845097bccp12c2ddjsn51a0858f137f"
+    }
+
+    conn.request("GET", "/statistics", headers=headers)
+
+    res = conn.getresponse()
+    data = res.read().decode('utf8')
+
+    entries = json.loads(data)['response']
+
+    for row in entries:
+        row_object = Stats.objects.filter(country=row['country'])
+        if row_object:
+            row_object.delete()
+        Stats.objects.create(
+            country = row['country'],
+            new_cases = row['cases']['new'],
+            active_cases = row['cases']['active'],
+            critical_cases = row['cases']['critical'],
+            recovered_cases = row['cases']['recovered'],
+            total_cases = row['cases']['total'],
+            new_deaths = row['deaths']['new'],
+            total_deaths = row['deaths']['total'],
+            total_tests = row['tests']['total'],
+            day = row['day'],
+            time = row['time']
+        )
---
+++ covid19Stats/stats/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Stats(models.Model):
+    country = models.TextField("country", primary_key=True)
+    new_cases = models.TextField("new_cases", null=True)
+    active_cases = models.IntegerField("active_cases", null=True)
+    critical_cases = models.IntegerField("critical_cases", null=True)
+    recovered_cases = models.IntegerField("recovered_cases", null=True)
+    total_cases = models.IntegerField("total_cases", null=True)
+    new_deaths = models.TextField('new_deaths', null=True)
+    total_deaths = models.IntegerField("total_deaths", null=True)
+    total_tests = models.IntegerField("total_tests", null=True)
+    day = models.TextField("day")
+    time = models.TextField("time")
