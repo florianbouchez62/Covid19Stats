9891aa0baed0adf086f5dd0e082e99bebecac854
Florian BOUCHEZ <florian.bouchez@epsi.fr>
2020-04-15 11:03:50 +0200

    ajout des pays uniquement dans la base

--- covid19Stats/scripts/get_data.py
+++ covid19Stats/scripts/get_data.py
@@ -13,6 +13,13 @@ def run():
     'x-rapidapi-key': "5bffb4691cmsh45cb38845097bccp12c2ddjsn51a0858f137f"
     }
 
+    conn.request("GET", "/countries", headers=headers)
+
+    res = conn.getresponse()
+    data = res.read().decode('utf8')
+
+    countries = json.loads(data)['response']
+
     conn.request("GET", "/statistics", headers=headers)
 
     res = conn.getresponse()
@@ -24,16 +31,17 @@ def run():
         row_object = Stats.objects.filter(country=row['country'])
         if row_object:
             row_object.delete()
-        Stats.objects.create(
-            country = row['country'],
-            new_cases = row['cases']['new'],
-            active_cases = row['cases']['active'],
-            critical_cases = row['cases']['critical'],
-            recovered_cases = row['cases']['recovered'],
-            total_cases = row['cases']['total'],
-            new_deaths = row['deaths']['new'],
-            total_deaths = row['deaths']['total'],
-            total_tests = row['tests']['total'],
-            day = row['day'],
-            time = row['time']
+        if row['country'] in countries:
+            Stats.objects.create(
+                country = row['country'],
+                new_cases = row['cases']['new'],
+                active_cases = row['cases']['active'],
+                critical_cases = row['cases']['critical'],
+                recovered_cases = row['cases']['recovered'],
+                total_cases = row['cases']['total'],
+                new_deaths = row['deaths']['new'],
+                total_deaths = row['deaths']['total'],
+                total_tests = row['tests']['total'],
+                day = row['day'],
+                time = row['time']
         )
